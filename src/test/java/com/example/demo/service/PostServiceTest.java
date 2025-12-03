f05b8040eeaec4f6f62c9d25ad215242472cc25c
Gotprgmer <gofiction95@gmail.com>
2025-12-03 21:43:44 +0900

    import 정리

--- src/test/java/com/example/demo/service/PostServiceTest.java
+++ src/test/java/com/example/demo/service/PostServiceTest.java
@@ -6,8 +6,6 @@ import com.example.demo.repository.PostEntity;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlGroup;
